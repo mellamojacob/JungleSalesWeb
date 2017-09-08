77f42b216446a53905347d1d6842f3392515e6a9
mellamojacob <development.virgin@gmail.com>
2017-09-08 12:42:23 -0600

    removed extra requirements

--- src/api/index.js
+++ src/api/index.js
@@ -37,10 +37,10 @@ app.listen(process.env.PORT || 5000);
 // require('./database');
 // require('./seed');
 
-app.use(parser.json());
+//app.use(parser.json());
 
 
-app.use('/api', router);
+//app.use('/api', router);
 
 app.listen(app.get('port'), function() {
 	console.log("The server is running on port ", app.get('port'))
