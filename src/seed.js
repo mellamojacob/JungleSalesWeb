32976f81b30e0a1a303637814ff5f7c035bfda4f
mellamojacob <development.virgin@gmail.com>
2017-09-13 17:24:22 -0600

    changed the start script to index.js

--- src/seed.js
+++ src/seed.js
@@ -1,21 +1,23 @@
 'use strict';
-/*
+
 var Company = require('./models/company');
 
-var companies = [
-	'Feed the dog',
-	'Walk the kids',
-	'Water the trees'
-];
+var comp
 
-companies.forEach(function (company, index) {
-	Company.find({'company_name': company }, function(err, companies) {
-		if (!err && !companies.length) {
-			Company.create({company_name: company});
-		}
-	});
-});
-*/
+// var companies = [
+// 	'Feed the dog',
+// 	'Walk the kids',
+// 	'Water the trees'
+// ];
+
+// companies.forEach(function (company, index) {
+// 	Company.find({'company_name': company }, function(err, companies) {
+// 		if (!err && !companies.length) {
+// 			Company.create({company_name: company});
+// 		}
+// 	});
+// });
+
 
 var User = require('./models/users');
 
