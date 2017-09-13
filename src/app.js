fbce912c1554093cf06305e312b8864b8db17c52
mellamojacob <development.virgin@gmail.com>
2017-09-13 17:00:56 -0600

    changed src/app.js files

--- src/app.js
+++ src/app.js
@@ -6,29 +6,33 @@ var router = require('./api');
 
 var app = express();
 
-app.listen(process.env.PORT || 5000);
+//app.listen(process.env.PORT || 5000);
 
 require('./database');
-require('./seed');
+// require('./seed');
 
-//app.use(parser.json());
+// //app.use(parser.json());
 
 
-app.use('/api', router);
+// app.use('/api', router);
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+// var MongoClient = require('mongodb').MongoClient
+//   , assert = require('assert');
 
-// Connection URL
-var url = 'mongodb://<jvirgin>:<Turing95>@ds011449.mlab.com:11449/heroku_w0m7vrgs';
+// // Connection URL
+// var url = 'mongodb://<jvirgin>:<Turing95>@ds011449.mlab.com:11449/heroku_w0m7vrgs';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
 
-  db.close();
-});
+//   db.close();
+// });
+
+//var app = express();
+
+app.listen(process.env.PORT || 5000);
 
 
 
